f4172dc365c3162315c8478170e8256848e8965f
Daru13 <camille.gobert@ens.fr>
2018-03-22 15:02:07 +0200

    Added menu building from server data test code.

--- src/ts/awm.ts
+++ src/ts/awm.ts
@@ -1,7 +1,26 @@
 import * as $ from "jquery";
+import { Menu } from "./Menus/Menu";
 
 $(document).ready(function () {
   console.log("AWM library initialised");
+  console.log("Loading the data...");
 
-  // TODO
+  $.getJSON("./example_data.json", null, function (data) {
+      console.log("Parsed data:");
+      console.log(data);
+
+      console.log("Building menu elements...");
+      let menus = [];
+
+      for (let menuData of data["menus"]) {
+        let menu = Menu.fromServerData(menuData);
+        menus.push(menu);
+      }
+
+      console.log(menus);
+    })
+    .fail(function (error) {
+      console.error("Unable to load the example JSON data:");
+      console.log(error);
+    });
 });
