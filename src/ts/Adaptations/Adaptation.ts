f3390f8167f290002bc361724fe55566a54bf547
Daru13 <camille.gobert@ens.fr>
2018-03-22 17:01:31 +0200

    Fixed wront path and reference to an old server-oriented type.

--- src/ts/Adaptations/Adaptation.ts
+++ src/ts/Adaptations/Adaptation.ts
@@ -1,5 +1,4 @@
 import { Menu } from "../Menus/Menu";
-import { ServerData } from "../ServerData/ServerDataManager";
 
 // Generic parent class for a menu adaptation
 // Any adaptation technique should extend this interface
@@ -7,5 +6,5 @@ export class Adaptation {
 
   // Apply the adaptation to a menu, using given data if required
   // This method must be extended to appropriately adapt the given menu!
-  static apply (menu: Menu, data: ServerData) { }
+  static apply (menu: Menu, data: object) { }
 }
