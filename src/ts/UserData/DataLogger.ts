a8a56ac56df255b2a46d609bf1b2a32e271eea50
Daru13 <camille.gobert@ens.fr>
2018-03-22 18:19:42 +0200

    Simple log features (clicks on menu items, current page).

--- src/ts/UserData/DataLogger.ts
+++ src/ts/UserData/DataLogger.ts
@@ -1,3 +1,64 @@
+import * as $ from "jquery";
+import { Database } from "./Database";
+
 export class DataLogger {
-  
+  // The database to use for logging
+  private database: Database;
+
+  // The callback method to use to log clicks on items
+  private itemClickCallback = (event) => { this.onMenuItemClick(event); };
+
+
+  constructor (database: Database) {
+    this.database = database;
+
+    this.start();
+  }
+
+  start () {
+    this.startListeningForItemClicks();
+    this.logCurrentPageVisit();
+  }
+
+  stop () {
+    this.stopListeningForPageLoads();
+  }
+
+  startListeningForItemClicks () {
+    console.log($("[data-awm-item]"));
+    $("[data-awm-item]").on("click", this.itemClickCallback);
+  }
+
+  stopListeningForPageLoads () {
+    $("[data-awm-item]").off("click", this.itemClickCallback);
+  }
+
+  onMenuItemClick (event: JQuery.Event) {
+    // Get the related item, group and menu tags
+    function getTag (name: string) {
+      return $(event.target).closest(`[data-awm-${name}]`).attr(`data-awm-${name}`);
+    }
+
+    let itemTag = getTag("item");
+    let groupTag = getTag("group");
+    let menuTag = getTag("menu");
+
+    // Also get the event timestamp and the current url pathname
+    let timestamp = event.timeStamp;
+    let pathname = window.location.pathname;
+
+    console.log(itemTag, groupTag, menuTag, timestamp, pathname);
+
+    // Log all this in the database
+    // TODO
+  }
+
+  logCurrentPageVisit () {
+    let pathname = window.location.pathname;
+
+    console.log(pathname);
+
+    // Log all this in the database
+    // TODO
+  }
 }
