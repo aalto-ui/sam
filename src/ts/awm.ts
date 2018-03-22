5d401cfa9bee3b8601c488d4ac3e0415b6a201a0
Daru13 <camille.gobert@ens.fr>
2018-03-22 18:19:58 +0200

    Test of the recently added log features.

--- src/ts/awm.ts
+++ src/ts/awm.ts
@@ -1,5 +1,7 @@
 import * as $ from "jquery";
 import { Menu } from "./Menus/Menu";
+import { DataLogger } from "./UserData/DataLogger";
+import { Database } from "./UserData/Database";
 
 $(document).ready(function () {
   console.log("AWM library initialised");
@@ -7,4 +9,7 @@ $(document).ready(function () {
   let menus = [];
   menus.push(Menu.fromSelectors("#awm-main-menu", ".menu-group", "li"));
   menus.push(Menu.fromSelectors("#awm-other-menu", "#awm-other-menu", "li"));
+  console.log(menus);
+
+  let logger = new DataLogger(new Database());
 });
