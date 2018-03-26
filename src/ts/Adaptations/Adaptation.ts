498f83c937b8d1ea6326cd037fcc868e1ab4d53e
Daru13 <camille.gobert@ens.fr>
2018-03-26 12:48:43 +0300

    Explicitely made adaptation an abstract class.

--- src/ts/Adaptations/Adaptation.ts
+++ src/ts/Adaptations/Adaptation.ts
@@ -2,7 +2,7 @@ import { Menu } from "../Menus/Menu";
 
 // Generic parent class for a menu adaptation
 // Any adaptation technique should extend this interface
-export class Adaptation {
+export abstract class Adaptation {
 
   // Apply the adaptation to a menu, using given data if required
   // This method must be extended to appropriately adapt the given menu!
