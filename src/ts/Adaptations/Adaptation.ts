3f027f3372197ff7b0bea0ca8ca90d483b774f38
Daru13 <camille.gobert@ens.fr>
2018-03-26 18:02:43 +0300

    The static apply method of an Adaptation now only expects an optional DataAnalyser object (and concerns the whole page).

--- src/ts/Adaptations/Adaptation.ts
+++ src/ts/Adaptations/Adaptation.ts
@@ -1,10 +1,11 @@
 import { Menu } from "../Menus/Menu";
+import { DataAnalyser } from "../UserData/DataAnalyser";
 
 // Generic parent class for a menu adaptation
 // Any adaptation technique should extend this interface
 export abstract class Adaptation {
 
-  // Apply the adaptation to a menu, using given data if required
-  // This method must be extended to appropriately adapt the given menu!
-  static apply (menu: Menu, data: object) { }
+  // Adapt the page menus, using the given data analyser if required
+  // This method must be implemented appropriately by any concrete child class!
+  static apply (analyser?: DataAnalyser) { }
 }
