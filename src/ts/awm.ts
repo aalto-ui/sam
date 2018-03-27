c6c541c7d57d0ce04390c9a42282071666aa50a1
Daru13 <camille.gobert@ens.fr>
2018-03-27 13:21:06 +0300

    Added a debug global function to reset the log database.

--- src/ts/awm.ts
+++ src/ts/awm.ts
@@ -5,6 +5,13 @@ import { Database } from "./UserData/Database";
 import { DataAnalyser } from "./UserData/DataAnalyser";
 import { HighlightMostClickedItems } from "./Adaptations/Highlighting/HighlightMostClickedItems";
 
+// For debug purposes: reset the log database
+this._db = <Database> null;
+window["emptyDatabase"] = function emptyDatabase () {
+  this._db.data = null;
+  localStorage.clear();
+}
+
 $(document).ready(function () {
   console.log("AWM library initialised");
 
@@ -13,7 +20,7 @@ $(document).ready(function () {
   menus.push(Menu.fromSelectors("#awm-other-menu", "#awm-other-menu", "li"));
   console.log("MENUS", menus);
 
-  let db = new Database();
+  let db = new Database(); this._db = db;
   let logger = new DataLogger(db);
   let analyser = new DataAnalyser(db);
 
