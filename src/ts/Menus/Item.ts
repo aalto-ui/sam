21aab3f6c5bb41042aaa0305a9cf697528a57f12
Daru13 <camille.gobert@ens.fr>
2018-04-06 16:46:52 +0300

    Added a method to find link elements from a menu item (w/ href filter).

--- src/ts/Menus/Item.ts
+++ src/ts/Menus/Item.ts
@@ -10,6 +10,28 @@ export class Item extends AdaptiveElement {
     super(node, selector, parent);
   }
 
+  // Look for link (<a>) nodes among the link element itself and is children,
+  // possibly fileterd by the given pathname
+  // (which must then match with the end of the href attribute of the link)
+  findLinkNodes (pathnameFilter?: string): JQuery {
+    let linkNodes = this.node.find("a");
+    if (this.node.is("a")) {
+      linkNodes = linkNodes.add(this.node);
+    }
+
+    // if a pathname filter is specified, only keep links whose href attribute
+    // match the end of the current page pathname
+    if (pathnameFilter) {
+      linkNodes = linkNodes.filter((_, element) => {
+        let href = $(element).attr("href");
+        return href.length > 0
+            && pathnameFilter.endsWith(href);
+      });
+    }
+
+    return linkNodes;
+  }
+
   static fromSelector (selector: string, parent: ItemGroup) {
     let node = parent.node.find(selector);
     return new Item(node, selector, parent);
