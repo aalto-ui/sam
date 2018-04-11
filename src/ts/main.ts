e110462e03041442206c54b2ef82a759ecfe9c29
Daru13 <camille.gobert@ens.fr>
2018-04-11 14:45:54 +0300

    Added an 'examples' directory for site-specific built standalone mods.
    Included a version for the English version of Wikipedia.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,11 +5,27 @@ import { AdaptiveWebMenus } from "./awm";
 
 $(document).ready(function () {
   // DEBUG: setup for page<1-6>.html
+
   let menuSelectors = {
     "#main-menu": {
       ".menu-group": "li"
     }
   };
+
+
+  // DEBUG: setup for en.wikipedia.org
+/*
+  let menuSelectors = {
+    "#mw-panel": {
+      "#p-navigation": "li",
+      "#p-interaction": "li",
+      "#p-tb": "li",
+      "#p-coll-print_export": "li",
+      "#p-wikibase-otherprojects": "li",
+      "#p-lang": "li"
+    }
+  };
+*/
 
   let awm = AdaptiveWebMenus.fromMenuSelectors(menuSelectors);
   console.log("AWM library initialised");
