2dbc1450b34bad1660c3b8d7c213243fb898273f
Daru13 <camille.gobert@ens.fr>
2018-04-18 14:36:46 +0300

    Added a parameter to cap the nb of highlighted items per group.

--- src/ts/Adaptations/Techniques/Highlight.ts
+++ src/ts/Adaptations/Techniques/Highlight.ts
@@ -4,14 +4,17 @@ import { AdaptiveElement } from "../../Elements/AdaptiveElement";
 import { Menu } from "../../Elements/Menu";
 import { ItemListPolicy } from "../Policies/ItemListPolicy";
 import { DataAnalyser } from "../../Data/DataAnalyser";
+import { Item } from "../../Elements/Item";
 
 
 export class Highlight implements AdaptationTechnique {
   private static readonly HIGHLIGHTED_ELEMENT_CLASS: string = "awm-highlighted";
 
-  // Maximum number of items to highlight
-  maxNbItems: number = 3;
+  // Global maximum number of items to highlight
+  maxNbHighlightedItems: number = 3;
 
+  // Maximum number of items to highlight per group
+  maxNbHighlightedItemsPerGroup: number = 2;
 
   constructor () { }
 
@@ -46,8 +49,14 @@ export class Highlight implements AdaptationTechnique {
 
   apply (menus: Menu[], policy: ItemListPolicy, analyser?: DataAnalyser) {
     let items = policy.getItemList(menus, analyser)
-      .slice(0, this.maxNbItems);
+      .slice(0, this.maxNbHighlightedItems);
 
-    Highlight.onAllElements(items);
+    let itemsSplitByGroup = Item.splitAllByGroup(items);
+    for (let sameGroupItems of itemsSplitByGroup) {
+      // Only keep at most maxNbDisplayedItemsPerGroup items
+      let remainingItems = sameGroupItems.splice(0, this.maxNbHighlightedItemsPerGroup);
+
+      Highlight.onAllElements(remainingItems);
+    }
   }
 }
