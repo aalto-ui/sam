4f762e2d5bab05183340d9e747adfad602a8cb9d
Daru13 <camille.gobert@ens.fr>
2018-07-20 16:12:13 +0300

    Refactored (+ cleaned) the Fold class.
    Again, done in a similar fashion than what has been already done 
    concerning the Highlight and Reorder techniques.
    
    A slight change in the DOM changes logic triggered an update of the 
    related CSS as well.

--- src/ts/Adaptations/Techniques/Fold.ts
+++ src/ts/Adaptations/Techniques/Fold.ts
@@ -8,97 +8,133 @@ import { Technique } from "./Technique";
 
 export class Fold implements Technique<Policy> {
 
+  private static readonly IS_FOLDED_CLASS: string = "awm-folded";
+  private static readonly SWITCH_FOLD_BUTTON_CLASS: string = "awm-fold-button";
+  private static readonly FOLDABLE_ELEMENT_CLASS: string = "awm-foldable";
+  private static readonly FOLDER_ELEMENT_CLASS: string = "awm-folder";
+
   readonly name: string = "Fold";
 
-  private static readonly FOLDED_CLASS: string = "awm-folded";
-  private static readonly FOLD_BUTTON_CLASS: string = "awm-fold-button";
-  private static readonly FOLDABLE_ELEMENT_CLASS: string = "awm-foldable";
-
-  private foldableItemParents: Set<HTMLElement>;
+  private folderNodes: JQuery[];
 
 
   constructor () {
-    this.foldableItemParents = new Set();
+    this.folderNodes = [];
   }
 
-  private static switchFoldState (parentNode: JQuery, foldButton: JQuery) {
-    if (parentNode.hasClass(Fold.FOLDED_CLASS)) {
-      parentNode.removeClass(Fold.FOLDED_CLASS);
-      foldButton.html("Show less");
+
+  private unfold (folderNode: JQuery) {
+    folderNode.removeClass(Fold.IS_FOLDED_CLASS);
+
+    folderNode
+      .find("." + Fold.SWITCH_FOLD_BUTTON_CLASS)
+      .html("Show less");
+  }
+
+  private fold (folderNode: JQuery) {
+    folderNode.addClass(Fold.IS_FOLDED_CLASS);
+
+    folderNode
+      .find("." + Fold.SWITCH_FOLD_BUTTON_CLASS)
+      .html("Show more");
+  }
+
+  private switchFoldState (folderNode: JQuery) {
+    if (folderNode.hasClass(Fold.IS_FOLDED_CLASS)) {
+      this.unfold(folderNode);
     }
     else {
-      parentNode.addClass(Fold.FOLDED_CLASS);
-      foldButton.html("Show more");
+      this.fold(folderNode);
     }
   }
 
-  private static createFoldButton (parentNode: JQuery): JQuery {
+
+  private createSwitchFoldButton (folderNode: JQuery): JQuery {
     let button = $("<button>")
-      .addClass(Fold.FOLD_BUTTON_CLASS)
+      .addClass(Fold.SWITCH_FOLD_BUTTON_CLASS)
       .attr("type", "button");
 
-    button.on("click", function () {
-      Fold.switchFoldState(parentNode, button);
+    // Handle clicks on the button to fold or unfold the folder
+    button.on("click", () => {
+      this.switchFoldState(folderNode);
     });
-
-    // TODO: REFACTOR
-    // By default, the menu is folded
-    Fold.switchFoldState(parentNode, button);
 
     return button;
   }
 
-  private appendFoldButtonsToAllFoldableItemParents () {
-    this.foldableItemParents.forEach((element) => {
-      let parentNode = $(element);
-      let foldButton = Fold.createFoldButton(parentNode);
 
-      $(element).append(foldButton);
-    });
+  private createAndAppendFoldButtonToFolder (folderNode: JQuery) {
+    let button = this.createSwitchFoldButton(folderNode);
+
+    // Append the button after the last folded item
+    folderNode
+      .find("." + Fold.FOLDABLE_ELEMENT_CLASS)
+      .last()
+      .after(button);
   }
 
-  private makeItemFoldable (item: Item) {
-    item.node.addClass(Fold.FOLDABLE_ELEMENT_CLASS);
-    this.foldableItemParents.add(item.node.parent()[0]);
-  }
-
-  private makeAllItemsFoldable (items: Item[]) {
+  private makeFolder (items: Item[]) {
+    // Tag items as foldables
     for (let item of items) {
-      this.makeItemFoldable(item);
+      item.node.addClass(Fold.FOLDABLE_ELEMENT_CLASS);
     }
+
+    // Tag the parent group node as the folder
+    let folderNode = items[0].parent.node;
+    folderNode.addClass(Fold.FOLDER_ELEMENT_CLASS);
+
+    this.folderNodes.push(folderNode);
+
+    // Append a fold button to the folder
+    this.createAndAppendFoldButtonToFolder(folderNode);
+
+    // By default, start with a folded folder
+    this.fold(folderNode);
   }
 
   reset () {
-    this.foldableItemParents.forEach((element) => {
-      let parent = $(element);
-      let children = parent.children();
+    for (let folderNode of this.folderNodes) {
+      // Remove foldable classes
+      folderNode
+        .find("." + Fold.FOLDABLE_ELEMENT_CLASS)
+        .removeClass(Fold.FOLDER_ELEMENT_CLASS);
 
-      children.removeClass(Fold.FOLDABLE_ELEMENT_CLASS);
-      parent.removeClass(Fold.FOLDED_CLASS);
-      parent.find("." + Fold.FOLD_BUTTON_CLASS).remove();
-    });
+      // Remove the switch fold button
+      folderNode
+        .find("." + Fold.SWITCH_FOLD_BUTTON_CLASS)
+        .remove();
 
-    this.foldableItemParents.clear();
+      // Remove the folder class
+      folderNode.removeClass(Fold.FOLDER_ELEMENT_CLASS);
+    }
+
+    this.folderNodes = [];
   }
 
   private getMaxNbItemsToDisplayInGroup (nbItemsInGroup: number): number {
     return Math.ceil(Math.sqrt(nbItemsInGroup));
   }
 
+  private splitAndApplyByGroup (items: Item[]) {
+    let topItemsSplitByGroup = Item.splitAllByGroup(items);
+
+    for (let sameGroupItems of topItemsSplitByGroup) {
+      this.applyInGroup(sameGroupItems);
+    }
+  }
+
+  private applyInGroup (sameGroupItems: Item[]) {
+    // Only keep items which must be moved into the folded menu (i.e. remove the top ones from the array)
+    let totalNbGroupItems = sameGroupItems[0].parent.items.length;
+    let nbItemToKeep = this.getMaxNbItemsToDisplayInGroup(totalNbGroupItems);
+    sameGroupItems.splice(0, nbItemToKeep);
+
+    this.makeFolder(sameGroupItems);
+  }
+
   apply (menus: Menu[], policy: Policy, analyser?: DataAnalyser) {
     let items = policy.getSortedItems(menus, analyser);
 
-    // Move items into folded menus independently for each group
-    let itemsSplitByGroup = Item.splitAllByGroup(items);
-    for (let sameGroupItems of itemsSplitByGroup) {
-      // Only keep items which must be moved into the folded menu (i.e. remove the top ones from the array)
-      let totalNbGroupItems = sameGroupItems[0].parent.items.length;
-      let nbItemToKeep = this.getMaxNbItemsToDisplayInGroup(totalNbGroupItems);
-      sameGroupItems.splice(0, nbItemToKeep);
-
-      this.makeAllItemsFoldable(sameGroupItems);
-    }
-
-    this.appendFoldButtonsToAllFoldableItemParents();
+    this.splitAndApplyByGroup(items);
   }
 }
